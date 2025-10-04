f8ddbdd76775b4b8d42144bd7cca146adab5a36e
KirillMitjurin <batja.top@gmail.com>
2025-10-05 00:22:54 +0300

    progress temp

--- art/MCsprites/back/playerData.gd
+++ art/MCsprites/back/playerData.gd
@@ -3,7 +3,7 @@ extends Node
 var speed := 200 
 var money := 200
 var bottles := 0 
-var heat := 20
+var heat := 100
 
 var upgrades = {
 	"speed": 0,
--- objects/player/player_hud.gd
+++ objects/player/player_hud.gd
@@ -3,9 +3,19 @@ class_name PlayerHud
 
 @export var bottle_counter: Label
 @export var temperature_bar: Label
+@onready var heat_bar = $temp/ProgressBar
+
 
 func update_bottle_count(): 
 	bottle_counter.text = str(PlayerData.bottles)
 	
-func update_temperature():
-	pass 
+
+func update_heat_bar(localheat: int):
+	heat_bar.value = localheat
+	var cold = Color(0, 0.4, 1)  
+	var hot = Color(1, 0, 0)      
+	var t = clamp(localheat / 100.0, 0.0, 1.0)
+	heat_bar.modulate = cold.lerp(hot, t)
+	
+func update_temperature(localheat: int):
+	temperature_bar.text = str(localheat)
