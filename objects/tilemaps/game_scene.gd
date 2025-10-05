7c7b01bdc9d8d98d2cad6b6ef0ccbc359c22a2cc
Kaur Huko Käämbre <kaurh.kaambre@gmail.com>
2025-10-05 14:42:02 +0300

    i forgot

--- objects/tilemaps/game_scene.gd
+++ objects/tilemaps/game_scene.gd
@@ -1,5 +1,7 @@
 extends Node2D
+class_name GameScene
 
+@export var fade_rect: ColorRect
 @onready var pause_menu = $CanvasLayer/PauseMenu
 var paused = false
 
@@ -10,6 +12,16 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
 	
+	if fade_rect:
+		fade_rect.modulate.a = 1.0
+		create_tween().tween_property(fade_rect, "modulate:a", 0.0, 2.0)
+
+func fade_scene_out(tree: SceneTree):
+	var tween = create_tween()
+	print("bruh 2")
+	tween.tween_property(fade_rect, "modulate:a", 1.0, 1.0)
+
+
 func pauseMenu():
 	if paused:
 		pause_menu.hide()
