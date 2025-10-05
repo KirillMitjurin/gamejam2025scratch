0e0eb8a8e15202116d3e56050ecce4fcdf990dd7
Kaur Huko Käämbre <kaurh.kaambre@gmail.com>
2025-10-05 12:46:42 +0300

    buying items done

--- scenes/ItemButton.gd
+++ scenes/ItemButton.gd
@@ -1,7 +1,7 @@
 extends TextureButton
 class_name ShopItem
 
-@export var item_name = "Item"
+@export var item_name = ""
 @export var cost = 0
 @export var speed_bonus_percent = 0
 @export var heat_bonus_percent = 0
@@ -10,18 +10,25 @@ class_name ShopItem
 @export var shop_ui: Control
 @export var next_item: ShopItem
 
-
 func _ready():
+	_on_hover_exit()
 	connect("mouse_entered", Callable(self, "_on_hover"))
 	connect("mouse_exited", Callable(self, "_on_hover_exit"))
 	connect("pressed", Callable(self, "_on_pressed"))
-	
+
+func _spec_text() -> String:
+	var text = ""
+	if (speed_bonus_percent != 0):
+		text = "Speed bonus: %d%%" % [speed_bonus_percent]
+	if (heat_bonus_percent != 0):
+		text = "Heat bonus: %d%%" % [heat_bonus_percent]
+	return text
 
 func _on_hover(): 
-	message_label.text = "Cost: %d" % [cost]
+	message_label.text = "Cost: %d\n%s" % [cost, _spec_text()]
 	
 func _on_hover_exit():
-	message_label.text = ""
+	message_label.text = item_name + "\n" + _spec_text()
 	
 func _on_pressed():
 	if PlayerData.bottles >= cost:
@@ -29,12 +36,18 @@ func _on_pressed():
 		
 		if (speed_bonus_percent != 0):
 			PlayerData.speed_multiplier = 1 + speed_bonus_percent / 100.0;
-			message_label.text = "%s purchased! New speed: %d%" % [item_name, (100 + speed_bonus_percent)]
+			message_label.text = "%s purchased! New speed: %d%%" % [item_name, 100 + speed_bonus_percent]
 		if (heat_bonus_percent != 0):
 			PlayerData.heat_multiplier = 1 + heat_bonus_percent / 100.0;
-			message_label.text = "%s purchased! New warmth: %d%%" % [item_name, 100 + heat_bonus_percent]
-			 
+			message_label.text = "%s purchased! New heat: %d%%" % [item_name, 100 + heat_bonus_percent]
+			
 		if shop_ui:
 			shop_ui.update_ui()
+		
+		if next_item:
+			next_item.show()
+		hide()
+			
+		
 	else:
-		message_label.text = "Not enough money"
+		message_label.text = "Not enough bottles"
