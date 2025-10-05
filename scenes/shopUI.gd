bb252e19821a83591e51bfa41b761bbf6547be52
Kaur Huko Käämbre <kaurh.kaambre@gmail.com>
2025-10-05 12:59:04 +0300

    monekying around

--- scenes/shopUI.gd
+++ scenes/shopUI.gd
@@ -11,5 +11,5 @@ func update_ui():
 	label_money.text = "Bottles: %d" %PlayerData.bottles
 	
 func _on_close_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/player_test.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	hide()
--- scenes/start_menu.gd
+++ scenes/start_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/player_test.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_exit_pressed() -> void:
